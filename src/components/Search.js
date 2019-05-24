b6bad943a8a72862f1c75d83e4f2eb86820d1265
Jorge Hinojosa <jorgexhinojosa@gmail.com>
2019-05-24 11:59:38 -0500

    Project setup complete

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./search.scss";
+
+function Search() {
+  return (
+    <div className="background">
+      <div className="header">
+        <h1>TuneFlix</h1>
+      </div>
+      <div className="body">
+        <p>Welcome to our page!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
