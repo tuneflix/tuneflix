ce391a8687495657cb058afd594751170211f876
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-05-30 13:34:30 -0500

    Results Show More Show Less In Progress

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -7,7 +7,52 @@ import Header from '../Shared/Header/Header'
 class Results extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentStart: 0,
+      currentEnd: 0,
+      movLength: 0,
+      movThruNum: 9,
+      movStyleMore: {},
+      movStyleLess: {},
+      tvStyle: {}
+    };
+  }
+
+  componentWillReceiveProps() {
+    if (this.props.resultsReducer.movResults.length<= 10) {
+      this.setState({movStyleMore: {display: 'none'}})
+      this.setState({movStyleLess: {display: 'none'}})
+    } else {
+      console.log('CDM: ', this.props.resultsReducer.movResults.length)
+      this.setState({movStyleMore: {}})
+      this.setState({movStyleLess: {}})
+    }
+  }
+
+  movShowMore = () => {
+      // console.log('in movShowMore')
+      this.setState({movThruNum: this.state.movThruNum + 10})
+      // console.log('movThruNum: ', this.state.movThruNum)
+      this.setState({movLength: this.props.resultsReducer.movResults.length})
+      // console.log('movLength: ', this.state.movLength)
+
+      if (this.state.movThruNum >= this.state.movLength || this.state.movLength <= 10 ) {
+        this.setState({movStyleMore: {display: 'none'}})
+      } else {
+        this.setState({movStyleMore: {}})
+      }
+  }
+
+  movShowLess = () => {
+    // console.log('in movShowMore')
+    this.setState({movThruNum: this.state.movThruNum - 10})
+    console.log('movThruNum: ', this.state.movThruNum)
+
+    if (this.state.movThruNum <= 10 || this.state.movLength <= 10 ) {
+      this.setState({movStyleLess: {display: 'none'}})
+    } else {
+      this.setState({movStyleLess: {}})
+    } 
   }
 
   render () {
@@ -16,9 +61,14 @@ class Results extends Component {
   // console.log(tvShowResults);
   
   let movNames = movResults.map((movie, index) => <Link to={`/results/movie/${movie.name}/${movie.id}`}><li key={index}>{movie.name}</li></Link> );
+  // console.log('movNames: ', movNames)
+
+  // 
+
   let tvNames = tvShowResults.map((tv, index) => <Link to={`/results/tvshow/${tv.name}/${tv.id}`}><li key={index}>{tv.name}</li></Link> )
-  ///WAS: results/tvshow/${tv.id}
-  //SHOULD BE: path="/results/tvshow/:tvShowName/:tvShowID"
+  
+  console.log('movNames length', movNames.length)
+  let showMovNames = movNames.slice(0, this.state.movThruNum); 
 
   //movies - handle if no results
   let displayMovNames = movNames;
@@ -38,17 +88,24 @@ class Results extends Component {
       <Header />
       <div className="results">
           <div className="movResults">
-            <h3>MOVIE RESULTS</h3>
-                <ul>
-                  {displayMovNames}
-                </ul>
-        </div>
-        <div className="tvResults">  
-          <h3>TV SERIES RESULTS</h3>
-                <ul>
-                  {displayTvNames}
-                </ul>
-        </div>
+              <h3>MOVIE RESULTS</h3>
+                 <div className='active'>
+                  <ul>
+                    {/* {displayMovNames} */}
+                    {showMovNames}
+                  </ul>
+                </div>
+                <button onClick={this.movShowMore} style={this.state.movStyleMore} >Show more . . .</button>
+                <button onClick={this.movShowLess} style={this.state.movStyleLess} >Show less . . .</button>
+          </div>
+          <div className="tvResults">  
+              <h3>TV SERIES RESULTS</h3>
+                <div className='tvActive'>
+                  <ul>
+                    {displayTvNames}
+                  </ul>
+                </div>
+          </div>
       </div>
     </div>
     );
