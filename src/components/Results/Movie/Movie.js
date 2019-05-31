aebd2500b968fbc8691f06e1416bc779439c1ed0
Jorge Hinojosa <jorgexhinojosa@gmail.com>
2019-05-31 16:28:04 -0500

    More Tv Show SCSS.  Filtered IMDB res in reducer.

--- src/components/Results/Movie/Movie.js
+++ src/components/Results/Movie/Movie.js
@@ -1,45 +1,46 @@
 import React from "react";
-import { connect } from "react-redux"
-import {getImage, getTvShows, getMovies,getMovieSongs} from "../../../ducks/resultsReducer"
-import SongCard from '../../Shared/SongCard/SongCard'
-import Header from '../../Shared/Header/Header'
-
+import { connect } from "react-redux";
+import {
+  getTvShowImdb,
+  getTvShows,
+  getMovies,
+  getMovieSongs
+} from "../../../ducks/resultsReducer";
+import SongCard from "../../Shared/SongCard/SongCard";
+import Header from "../../Shared/Header/Header";
 
 class Movie extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state ={
-      
-    }
+    this.state = {};
   }
 
-  componentDidMount(){
-    const movID = this.props.match.params.movID
-    const movName = this.props.match.params.movName
+  componentDidMount() {
+    const movID = this.props.match.params.movID;
+    const movName = this.props.match.params.movName;
     this.props.getMovieSongs(movID);
-    // this.props.getImage(movName)
-    
+    // this.props.getTvShowImdb(movName)
   }
-  
-  
-  render(){
-    const{ movResults,image,songResults} = this.props.resultsReducer
+
+  render() {
+    const { movResults, image, songResults } = this.props.resultsReducer;
     console.log(image);
-    console.log(songResults)
-    
-   
+    console.log(songResults);
+
     return (
-      <div className="Error" >
-          <Header />
-          <SongCard />
+      <div className="Error">
+        <Header />
+        <SongCard />
       </div>
     );
-
   }
 }
-function mapStateToProps (state){
-  return{
+function mapStateToProps(state) {
+  return {
     resultsReducer: state.resultsReducer
-  }
+  };
 }
-export default connect(mapStateToProps, {getMovies,getImage,getMovieSongs})(Movie);
+export default connect(
+  mapStateToProps,
+  { getMovies, getTvShowImdb, getMovieSongs }
+)(Movie);
