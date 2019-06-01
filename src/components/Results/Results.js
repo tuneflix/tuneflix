33be563c907d5a6bd5b200d09eb9e37f65f98f08
Theresa Stocking <nottodayjose@MacBook-Pro.attlocal.net>
2019-05-31 23:06:26 -0500

    Show More and Less Buttons Are Working

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -7,7 +7,8 @@ import Header from "../Shared/Header/Header"
 class Results extends Component {
   constructor(props) {
     super(props);
-      this.underShowMoreButton = React.createRef();
+      this.underMovShowMoreButton = React.createRef();
+      this.underTvShowMoreButton = React.createRef();
     this.state = {
       currentStart: 0,
       currentEnd: 0,
@@ -15,69 +16,75 @@ class Results extends Component {
       movThruNum: 10,
       movStyleMore: {},
       movStyleLess: {},
+      tvLength: 0,
+      tvThruNum: 10,
+      tvStyleMore: {},
+      tvStyleLess: {},
       tvStyle: {},
     };
   }
-
-  componentDidMount() {
-    console.log('CDM')
-    console.log('movThruNum CDM: ', this.state.movThruNum);
-    console.log('movLength CDM: ', this.state.movLength);
-  }
   
-  componentWillReceiveProps() {
-    this.setState({movLength: this.props.resultsReducer.movResults.length})
-    //this.props.resultsReducer
-  }
-
-  focusMovShowMoreBtn () {
-    // this.movShowMoreBtn.current.focus();
-    // document.getElementById('showMovMore').focus();
+ componentDidUpdate(prevProps) {
+    if (prevProps.resultsReducer !== this.props.resultsReducer) {
+      this.setState({movLength: this.props.resultsReducer.movResults.length})
+      this.setState({tvLength: this.props.resultsReducer.tvShowResults.length})
+    }
   }
 
   movShowMore = () => {
     this.setState({movThruNum: this.state.movThruNum + 10});
-    console.log('movThruNum MORE: ', this.state.movThruNum);
-    console.log('movLength: ', this.state.movLength);
     this.scrollIntoView_movBtnShowMore();
   }
-
-  // scrollToMyRef = () => window.scrollTo(0, (this.myRef.current.offsetTop-60)) 
+  
+  movShowLess = () => {
+    this.setState({movThruNum: this.state.movThruNum - 10})
+    this.scrollIntoView_movBtnShowMore();
+  }
 
   scrollIntoView_movBtnShowMore = () => {
     setTimeout(() => {
-      this.underShowMoreButton.current.scrollIntoView({block: 'end', behavior: 'smooth'});
-    }, 50);
+      this.underMovShowMoreButton.current.scrollIntoView({block: 'end', behavior: 'smooth'});
+    }, 75);
   }
 
-  movShowLess = () => {
-    this.setState({movThruNum: this.state.movThruNum - 10})
-    console.log('movThruNum LESS: ', this.state.movThruNum);
-    this.scrollIntoView_movBtnShowMore();
+  tvShowMore = () => {
+    this.setState({tvThruNum: this.state.tvThruNum + 10});
+    this.scrollIntoView_tvBtnShowMore();
   }
+  
+  tvShowLess = () => {
+    this.setState({tvThruNum: this.state.tvThruNum - 10});
+    this.scrollIntoView_tvBtnShowMore();
+  }
+
+  scrollIntoView_tvBtnShowMore = () => {
+    setTimeout(() => {
+      this.underTvShowMoreButton.current.scrollIntoView({block: 'end', behavior: 'smooth'});
+    }, 75);
+  }
+
 
   render () {
 
-  const{ movResults, tvShowResults, image} = this.props.resultsReducer;
+  const{ movResults, tvShowResults} = this.props.resultsReducer;
 
   let movNames = movResults.map((movie, index) => <Link to={`/results/movie/${movie.name}/${movie.id}`}><li id={`mov${index}`} key={index}>{movie.name}</li></Link> );
 
-  let tvNames = tvShowResults.map((tv, index) => <Link to={`/results/tvshow/${tv.name}/${tv.id}`}><li key={index}>{tv.name}</li></Link> )
+  let tvNames = tvShowResults.map((tv, index) => <Link to={`/results/tvshow/${tv.name}/${tv.id}`}><li id={`tv${index}`} key={index}>{tv.name}</li></Link> )
   
-  console.log('movNames length', movNames.length)
   let showMovNames = movNames.slice(0, this.state.movThruNum); 
+  let showTvNames = tvNames.slice(0, this.state.tvThruNum);
 
     //movies - handle if no results
     let displayMovNames = movNames;
     if (movNames.length === 0) {
-      // displayMovNames = `Sorry - there were no results.`;
       showMovNames = `Sorry - there were no results.`;
     }
 
     //movies - handle if no results
     let displayTvNames = tvNames;
     if (tvNames.length === 0) {
-      displayTvNames = `Sorry - there were no results.`;
+      showTvNames = `Sorry - there were no results.`;
     }
 
     return (
@@ -87,18 +94,18 @@ class Results extends Component {
         <div className="results">
           <div className="movResults">
               <h3>MOVIE RESULTS</h3>
-                 <div className='active'>
+                 <div className='movActive'>
                   <ul>
                     {/* {displayMovNames} */}
                     {showMovNames}
                   </ul>
                 </div>
                 <div className='movButtons'>
-                {movResults.length > 10 && ((this.state.movThruNum + 10) < this.state.movLength) ? 
+                {movResults.length > 10 && ((this.state.movThruNum + 10) < (this.state.movLength+9)) ? 
                 <div id='movShowMore'>
                   <button id='movShowMoreBtn' onClick={this.movShowMore} style={this.state.movStyleMore}>Show more . . .</button>
                 </div> : null}
-                  <div id='underShowMoreButton' ref={this.underShowMoreButton}></div>
+                  <div id='underMovShowMoreButton' ref={this.underMovShowMoreButton}></div>
                 {this.state.movThruNum > 11 ?
                 <div>
                   <button onClick={this.movShowLess} style={this.state.movStyleLess} >Show less . . .</button>
@@ -109,9 +116,21 @@ class Results extends Component {
               <h3>TV SERIES RESULTS</h3>
                 <div className='tvActive'>
                   <ul>
-                    {displayTvNames}
+                    {/* {displayTvNames} */}
+                    {showTvNames}
                   </ul>
                 </div>
+                <div className='tvButtons'>
+                {tvShowResults.length > 10 && ((this.state.tvThruNum + 10) < (this.state.tvLength + 9)) ? 
+                <div id='tvShowMore'>
+                  <button id='tvShowMoreBtn' onClick={this.tvShowMore} style={this.state.tvStyleMore}>Show more . . .</button>
+                </div> : null}
+                  <div id='underTvShowMoreButton' ref={this.underTvShowMoreButton}></div>
+                {this.state.tvThruNum > 11 ?
+                <div>
+                  <button onClick={this.tvShowLess} style={this.state.tvStyleLess} >Show less . . .</button>
+                </div> : null}
+                </div>                
           </div>
       </div>
     </div>
