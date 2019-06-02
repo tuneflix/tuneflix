7a6f1b2999280b26429efa74e7706b994e3c52b4
Fethi E <jack@jacks-MacBook-Pro-2.local>
2019-06-01 22:49:36 -0500

    error page is done

--- src/components/Shared/Error/Error.js
+++ src/components/Shared/Error/Error.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Error.scss";
-
+import Header from "../Header/Header"
 
 function Error() {
   return (
     <div className="Error">
+      <Header />
+      <div className = "error-content">
       
+        <h1>404 Page Not Found</h1>
+        
+        <video muted autoPlay="autoPlay" playsInline="playsInline" className="video-tag">
+          <source src="https://developers4good.s3.us-east-2.amazonaws.com/bucketFolder/final_gandalf.mp4" />
+        </video>
+
+      </div>
+
     </div>
   );
 }
